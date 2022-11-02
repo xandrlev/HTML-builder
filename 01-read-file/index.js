9c963edd099da104c22f0748f5a3fd075d890bb6
xandrlev <xandrlev@gmail.com>
2022-11-02 10:44:07 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'));
+// const stream = new fs.ReadStream(path.join(__dirname, "text.txt"), {
+//   encoding: "utf-8",
+// });
+
+stream.on("readable", () => {
+  const data = stream.read();
+  if (data != null) console.log(data.toString());
+  
+});
+
+stream.on('error', (err) => {
+  if(err) throw err;
+})

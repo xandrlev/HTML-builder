284700771f15eece2b2aaf982443f0a29bd9efd1
xandrlev <xandrlev@gmail.com>
2022-11-07 23:31:41 +0300

    feat: start 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,3 +6,40 @@
 СПАСИБО!!!
 
 */
+
+const fs = require('fs');
+const fsPromise = require('fs/promises');
+const path = require('path');
+const destDir = path.join(__dirname, 'project-dist');
+
+(async function buildPage () {
+  await createFolder(destDir);
+  await createFolder(path.join(destDir, 'assets'));
+
+  await createFile(path.join(destDir, 'index.html'));
+  await createFile(path.join(destDir, 'style.css'));
+
+  copyFiles();
+
+})()
+
+function createFolder (path) {
+  fs.mkdir(path, err => {
+    if(err) throw err;
+  });
+}
+
+function createFile (path) {
+  fs.open((path),'a+', err => {
+    if(err) throw err;
+  });
+}
+
+async function copyFiles () {
+  const files = await fsPromise.readdir(path.join(__dirname, 'assets'));
+
+  files.forEach((file) => {
+    console.log(file);
+    
+  })
+}
